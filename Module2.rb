869614a31608925ba615b35e1ba2221a27866a50
Archita Arakka <architaarakka12@gmail.com>
2023-02-22 10:51:19 +0530

    Program1

--- Module2.rb
+++
@@ -1,4 +0,0 @@
-require_relative "Module.rb"
-
-include Tools
-Tools.sayhi("Archita")
--- csv.rb
+++
@@ -1,7 +0,0 @@
-cats = [
-    [:blue, 1],
-    [:white, 2],
-    [:black_and_white, 3]
-]
-
-cats.map {|c| c.join(",")}.join("\n")
